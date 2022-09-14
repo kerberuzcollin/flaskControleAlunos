af5b84102e4f8a36f473185a233e8fd794b7c3b5
kerberuzcollin <kerberuz.collin@gmail.com>
2022-09-14 20:12:18 -0300

    SQLALCHEMY

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from .extensions import db, migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app)
+
+    return app
